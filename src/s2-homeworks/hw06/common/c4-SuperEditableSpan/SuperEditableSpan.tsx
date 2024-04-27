b11040e81b6f1a7f6601e6edc2ab128bbfd3d1af
NovikovRomanV <rimm19845@gmail.com>
2024-04-27 22:13:34 +0300

    homework 7

--- src/s2-homeworks/hw06/common/c4-SuperEditableSpan/SuperEditableSpan.tsx
+++ src/s2-homeworks/hw06/common/c4-SuperEditableSpan/SuperEditableSpan.tsx
@@ -44,6 +44,7 @@ const SuperEditableSpan: React.FC<SuperEditableSpanType> = (
         // выключить editMode при нажатии Enter // делают студенты
         setEditMode(false)
         onEnter?.()
+
     }
     const onBlurCallback = (e: React.FocusEvent<HTMLInputElement>) => {
         // выключить editMode при нажатии за пределами инпута // делают студенты
